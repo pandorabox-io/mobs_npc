2a15d5d071e427d4db3993f47957b711d914c104
OgelGames <olliverdc28@gmail.com>
2020-09-22 21:53:05 +1000

    tidy up a bit

--- init.lua
+++ init.lua
@@ -1,74 +1,19 @@
 
 -- Load support for intllib.
 local path = minetest.get_modpath(minetest.get_current_modname())
-local S = minetest.get_translator and minetest.get_translator("mobs_npc") or
-		dofile(path .. "/intllib.lua")
+local S = minetest.get_translator and minetest.get_translator("mobs_npc") or dofile(path .. "/intllib.lua")
 mobs.intllib = S
 
-function mobs.npc_order(self, clicker, mob_name)
-
-	local name = clicker:get_player_name()
-
-	if self.owner and self.owner == name then
-
-		mob_name = (self.nametag and self.nametag ~= "") and self.nametag or mob_name
-
-		self.attack = nil
-
-		if self.order == "follow" then
-
-			self.order = "stand"
-
-			self.state = "stand"
-			self:set_animation("stand")
-			self:set_velocity(0)
-
-			minetest.chat_send_player(name, S("@1 stands still.", mob_name))
-		else
-			self.order = "follow"
-
-			minetest.chat_send_player(name, S("@1 will follow you.", mob_name))
-		end
-
-		return true
-	end
-
-	return false
-end
-
-function mobs.npc_drop(self, clicker, mob_name, drops)
-
-	local item = clicker:get_wielded_item()
-
-	if item:get_name() == "default:gold_lump" then
-
-		local name = clicker:get_player_name()
-
-		mob_name = (self.nametag and self.nametag ~= "") and self.nametag or mob_name
-
-		if not mobs.is_creative(name) then
-			item:take_item()
-			clicker:set_wielded_item(item)
-		end
-
-		local pos = self.object:get_pos()
-
-		pos.y = pos.y + 0.5
-
-		minetest.add_item(pos, { name = drops[math.random(#drops)] })
-
-		minetest.chat_send_player(name, S("@1 dropped you an item for gold!", mob_name))
-
-		return true
-	end
-
-	return false
-end
+-- Helper functions
+dofile(path .. "/functions.lua")
 
 -- NPCs
 dofile(path .. "/npc.lua") -- TenPlus1
-dofile(path .. "/trader.lua")
 dofile(path .. "/igor.lua")
+dofile(path .. "/trader.lua")
+
+-- Trader items
+dofile(path .."/trader_items.lua")
 
 -- Lucky Blocks
 dofile(path .. "/lucky_block.lua")
--- trader.lua
+++ trader.lua
@@ -18,47 +18,9 @@ mobs.trader = {
 
 	items = {
 		-- {item, currency, min price, max price, daily stock, rarity}
-		{"default:apple 10",			"default:gold_ingot",		1,		3,		30,		10},
-		{"farming:bread 10",			"default:gold_ingot",		1,		4,		30,		5},
-		{"default:clay 10",				"default:gold_ingot",		2,		5,		100,	12},
-		{"default:brick 10",			"default:gold_ingot",		2,		6,		100,	17},
-		{"default:glass 10",			"default:gold_ingot",		1,		4,		100,	17},
-		{"default:obsidian 10",			"default:gold_ingot",		5,		15,		60,		50},
-		{"default:diamond 1",			"default:mese_crystal",		1,		3,		25,		40},
-		{"default:diamond 1",			"default:gold_ingot",		5,		10,		25,		40},
-		{"farming:wheat 10",			"default:gold_ingot",		1,		4,		50,		17},
-		{"default:tree 10",				"default:gold_ingot",		1,		4,		80,		20},
-		{"default:stone 20",			"default:gold_ingot",		1,		3,		100,	17},
-		{"default:desert_stone 10",		"default:gold_ingot",		1,		4,		100,	27},
-		{"default:sapling 5",			"default:gold_ingot",		1,		3,		30,		7},
-		{"default:pick_steel 1",		"default:gold_ingot",		1,		4,		10,		7},
-		{"default:sword_steel 1",		"default:gold_ingot",		1,		3,		10,		17},
-		{"default:shovel_steel 1",		"default:gold_ingot",		1,		3,		10,		17},
-		{"default:cactus 5",			"default:gold_ingot",		2,		5,		30,		40},
-		{"default:papyrus 10",			"default:gold_ingot",		2,		6,		50,		40},
-		{"default:mese_crystal 1",		"default:dirt_with_grass",	6,		10,		30,		90},
-		{"default:mese_crystal 1",		"default:gold_ingot",		3,		6,		30,		80},
-		{"default:sandstone 10",		"default:gold_ingot",		1,		4,		80,		20},
-		{"default:dirt 10",				"default:gravel",			8,		12,		80,		15},
-		{"default:dirt 10",				"default:gold_ingot",		1,		3,		80,		10},
-		{"bucket:bucket_water 1",		"default:gold_ingot",		1,		3,		10,		20},
-		{"bucket:bucket_river_water 1",	"default:gold_ingot",		2,		4,		10,		30},
-		{"bucket:bucket_lava 1",		"default:gold_ingot",		3,		8,		10,		50},
+		-- see trader_items.lua
 	}
 }
-
--- check that all trade items are good to use
-minetest.register_on_mods_loaded(function()
-	for _,v in pairs(mobs.trader.items) do
-		assert(type(v[1]) == "string" and
-			type(v[1]) == "string" and
-			type(v[3]) == "number" and
-			type(v[4]) == "number" and
-			type(v[5]) == "number" and
-			type(v[6]) == "number"
-			,"invalid mobs trader item")
-	end
-end)
 
 local function set_random_name(self)
 	local name = tostring(mobs.trader.names[math.random(#mobs.trader.names)])
